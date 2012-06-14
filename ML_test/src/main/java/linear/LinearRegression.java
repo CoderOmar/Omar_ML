b5a0358a9afcb18618a42016fae3b75a88aa5788
CoderOmar <omar@bit>
2012-06-14 16:53:43 +0800

    init project

---
+++ ML_test/src/main/java/linear/LinearRegression.java
@@ -0,0 +1,14 @@
+package linear;
+
+public class LinearRegression {
+
+	/**
+	 * @param args
+	 * Jun 14, 2012
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
